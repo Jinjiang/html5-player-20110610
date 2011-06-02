5b456933ee570912590b5ec45e0e95a21c5ce6db
Jinjiang Zhao <zhaojinjiang@yahoo.com.cn>
2011-06-02 17:15:36 +0800

    added cymbals

---
+++ cymbals/src/cymbals.js
@@ -0,0 +1,50 @@
+﻿var ws;
+var type = 'cymbals';
+var host = '192.168.1.57'; // 'localhost';
+var notSendFlag = false;
+var z = 0;
+
+
+
+
+function init() {
+    resize();
+    $(window).bind('resize', resize);
+    if (window.WebSocket) {
+        ws = new WebSocket('ws://' + host + ':8080/');
+        ws.onopen = function () {
+            ws.send('client:cymbals');
+        };
+    }
+    
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', function (event) {
+            z = Math.abs(Math.floor(event.acceleration.z * 1000));
+            
+            if ((z > 9000) && !notSendFlag) {
+                send(1);
+                notSendFlag = true;
+            }
+            else if ((z < 5000) && notSendFlag) {
+                notSendFlag = false;
+            }
+            
+        }, true);
+    }
+    
+}
+
+
+
+
+function send(value) {
+    ws.send(type + ':' + value);
+}
+function resize() {
+    $('table').css('height', $(window).height() + 'px');
+}
+
+
+
+
+$(init);
